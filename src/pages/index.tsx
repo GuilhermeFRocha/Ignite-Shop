7505186678f3cca2a1d1abd14414df68c31705a9
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-03-21 21:51:14 -0300

    add stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,55 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled ('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import {useKeenSlider} from 'keen-slider/react'
 
-  span: {
-    fontWeight: 'bold'
-  },
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
 
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
- 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides : {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <Button> Enviar </Button>
-  )
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} alt="camiseta" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} alt="camiseta" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$89,90</span>
+        </footer>
+      </Product>
+
+       <Product className="keen-slider__slide">
+        <Image src={camiseta2} alt="camiseta" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$89,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="camiseta" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$89,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
